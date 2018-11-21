6314127e3e5a0036c93f963e19152cc5d5db2400
krishnanselva <krishnanselva@yahoo.com>
2018-11-21 18:50:15 +0000

    refactoring again

--- test/web-crawler-rxjs.spec.js
+++ test/web-crawler-rxjs.spec.js
@@ -3,12 +3,12 @@ var chai = require('chai');
 var chaiRx = require('chai-rx');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var webCrawler = require('../routes/web-crawler-rxjs');
-var baseUrl = 'https://wiprodigital.com';
+var baseUrl = 'https://abcd.com';
 var siteMapfile = 'test/sitemap_test.txt';
 describe('Unittest webCrawler', function () {
   let webCrawlerTest = new webCrawler(baseUrl, siteMapfile);
   it(`convert absolute url if it is relative`, function () {
-    expect(webCrawlerTest.addToSiteMap(baseUrl, '/test')).to.be.equals('https://wiprodigital.com/test');
+    expect(webCrawlerTest.addToSiteMap(baseUrl, '/test')).to.be.equals('https://abcd.com/test');
   });
   it(`skip changing the url if it is from different domain`, function () {
     expect(webCrawlerTest.addToSiteMap(baseUrl, 'http://abc.com/test')).to.be.equals('http://abc.com/test');
